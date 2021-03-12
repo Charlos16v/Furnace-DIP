87907b8c83462e7c419eec62050eb554e20ce896
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-03-12 16:35:40 +0100

    feat(Torch): created class Torch that implements Heater

---
+++ src/main/java/edu/pingpong/furnace/otherstuff/Torch.java
@@ -0,0 +1,42 @@
+package edu.pingpong.furnace.otherstuff;
+
+import edu.pingpong.furnace.interfaces.Heater;
+import edu.pingpong.furnace.types.RoomTemperature;
+
+public class Torch implements Heater {
+
+    @Override
+    public void engage(RoomTemperature temperature) {
+        temperature.incrementTemperature(10);
+    }
+
+    @Override
+    public void disengage(RoomTemperature temperature) {
+        temperature.incrementTemperature(-1);
+    }
+
+    public void saySomething(){
+        System.out.println("\n"
+        + "\t           /|" + "\n"
+        + "\t        /\\/ |/\\" + "\n"
+        + "\t        \\  ^   | /\\  /\\ " + "\n"
+        + "\t  (\\/\\  / ^   /\\/  )/^ )" + "\n"
+        + "\t   \\  \\/^ /\\       ^  /" + "\n"
+        + "\t    )^       ^ \\     (" + "\n"
+        + "\t   (   ^   ^      ^\\  )" + "\n"
+        + "\t    \\___\\/____/______/" + "\n"
+        + "\t    [________________]" + "\n"
+        + "\t     |              |" + "\n"
+        + "\t     |     //\\\\     |" + "\n"
+        + "\t     |    <<()>>    |" + "\n"
+        + "\t     |     \\\\//     |" + "\n"
+        + "\t      \\____________/" + "\n"
+        + "\t          |    |" + "\n"
+        + "\t          |    |" + "\n"
+        + "\t          |    |" + "\n"
+        + "\t          |    |" + "\n"
+        + "\t          |    |" + "\n"
+        + "\t          |    |" + "\n" + "\n"
+        + "\t The Torch says... fire!");
+    }
+}
