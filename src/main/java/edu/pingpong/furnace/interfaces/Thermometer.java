ff701871eb5af8dc41751b1dfd195ca798315c0d
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-03-12 11:56:30 +0100

    feat(Thermomether): implement read() method

--- src/main/java/edu/pingpong/furnace/interfaces/Thermometer.java
+++ src/main/java/edu/pingpong/furnace/interfaces/Thermometer.java
@@ -1,4 +1,8 @@
 package edu.pingpong.furnace.interfaces;
 
+import edu.pingpong.furnace.types.RoomTemperature;
+
 public interface Thermometer {
+
+    public double read(RoomTemperature temperature);
 }
