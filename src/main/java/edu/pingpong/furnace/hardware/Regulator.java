c6831e0ae57e2629fdd38365f74d0cec0246fbf5
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-03-12 13:45:11 +0100

    feat(Regulator): implemented regulate & showMessage method

--- src/main/java/edu/pingpong/furnace/hardware/Regulator.java
+++ src/main/java/edu/pingpong/furnace/hardware/Regulator.java
@@ -1,5 +1,42 @@
 package edu.pingpong.furnace.hardware;
 
 
+import edu.pingpong.furnace.interfaces.Heater;
+import edu.pingpong.furnace.interfaces.Thermometer;
+import edu.pingpong.furnace.types.RegulatorDisplayCodes;
+import edu.pingpong.furnace.types.RoomTemperature;
+
 public class Regulator {
+
+    public void regulate(
+            Thermometer thermometer, Heater heater,
+                         double minTemp, double maxTemp, RoomTemperature temperature
+    ) {
+        RegulatorDisplayCodes code;
+        while (thermometer.read(temperature) < maxTemp) {
+            code = RegulatorDisplayCodes.HEATING;
+            heater.engage(temperature);
+            showMessage(code, temperature);
+        }
+
+        while (thermometer.read(temperature) > minTemp) {
+            code = RegulatorDisplayCodes.WAITING;
+            heater.disengage(temperature);
+            showMessage(code, temperature);
+        }
+    }
+
+    private void showMessage(RegulatorDisplayCodes code, RoomTemperature temperature) {
+        switch (code) {
+            case WAITING:
+                System.out.println("WAITING... Temperature => " + temperature.getTemperature());
+                break;
+            case HEATING:
+                System.out.println("HEATING... Temperature => " + temperature.getTemperature());
+                break;
+            default:
+                System.out.println("Something strange it's happening...");
+                break;
+        }
+    }
 }
