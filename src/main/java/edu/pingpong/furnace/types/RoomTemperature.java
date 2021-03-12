1114692f274716bbd27b71dd2c8e1d8bda829edc
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-03-12 11:31:33 +0100

    feat(RoomTemperature): Singleton pattern applied with getInstance method

--- src/main/java/edu/pingpong/furnace/types/RoomTemperature.java
+++ src/main/java/edu/pingpong/furnace/types/RoomTemperature.java
@@ -2,10 +2,20 @@ package edu.pingpong.furnace.types;
 
 public class RoomTemperature {
 
+    // instance for the Singleton
+    public static RoomTemperature instance;
     private double temperature = 0d;
 
-    public RoomTemperature(double temperature) {
+    private RoomTemperature(double temperature) {
         this.temperature = temperature;
+    }
+
+    // Singleton getInstance
+    public static RoomTemperature getInstance(double temperature){
+        if (instance == null){
+            instance = new RoomTemperature(temperature);
+        }
+        return instance;
     }
 
     public void setTemperature(double temperature) {
