877df8c38d473ead56057d2518af371891bd07fd
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-03-12 11:59:41 +0100

    feat(Heater): implemented engage & disengage methods

--- src/main/java/edu/pingpong/furnace/interfaces/Heater.java
+++ src/main/java/edu/pingpong/furnace/interfaces/Heater.java
@@ -1,4 +1,9 @@
 package edu.pingpong.furnace.interfaces;
 
+import edu.pingpong.furnace.types.RoomTemperature;
+
 public interface Heater {
+
+    public void engage(RoomTemperature temperature);
+    public void disengage(RoomTemperature temperature);
 }
