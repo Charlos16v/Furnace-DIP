ababfacc4bd06952a64e3f8804a3025e6ce44261
Charlos16v <cdominguez@cifpfbmoll.eu>
2021-03-12 11:56:59 +0100

    feat(RemoteCommandSensor): overrided read() method

--- src/main/java/edu/pingpong/furnace/hardware/RemoteCommandSensor.java
+++ src/main/java/edu/pingpong/furnace/hardware/RemoteCommandSensor.java
@@ -1,6 +1,12 @@
 package edu.pingpong.furnace.hardware;
 
 import edu.pingpong.furnace.interfaces.Thermometer;
+import edu.pingpong.furnace.types.RoomTemperature;
 
 public class RemoteCommandSensor implements Thermometer {
+
+    @Override
+    public double read(RoomTemperature temperature) {
+        return temperature.getTemperature();
+    }
 }
